2baba1085fc563eb098f755129b7bdfb5e853aa9
Jesper Larsson <jesper.mailen@gmail.com>
2022-05-04 15:38:49 +0200

    Menyer 1.0

---
+++ Assets/Scripts/ClickButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ClickButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
