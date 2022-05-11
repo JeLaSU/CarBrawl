9ac49c4396796ce4c9a593722379d7f488333f59
Nyzellan <lovisa.nyz@gmail.com>
2022-05-11 09:54:26 +0200

    dads

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        faceMouse();
+        
+    }
+    void faceMouse() // gör så att vapnets ände följer musen
+    {
+        Vector2 mousePosition = Input.mousePosition; 
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition); // initierar mousePosition till musen 
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y); 
+        transform.up = direction; // eftersom det är 2D så blir up, direction
+    }
+    
+}
