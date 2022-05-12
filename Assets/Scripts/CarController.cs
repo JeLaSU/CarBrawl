f04a85414b57c54829e1a5f063ee74a462f2d7c7
Nyzellan <lovisa.nyz@gmail.com>
2022-05-12 12:06:05 +0200

    lvl editor??
    skulle göra boomerang råkade göra lvl editor

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -7,16 +7,16 @@ public class CarController : MonoBehaviour
     Rigidbody2D rigidbody2D;
     public float speed;
 
+   
     // Start is called before the first frame update
     void Awake()
     {
          rigidbody2D = GetComponent<Rigidbody2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        speed = rigidbody2D.velocity.sqrMagnitude;
+        speed = rigidbody2D.velocity.sqrMagnitude;       
     }
 }
