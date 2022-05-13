a163d97995ab2bd675ccb50dab38eb6eb2328a01
Mindeusa <89967008+Mindeusa@users.noreply.github.com>
2022-05-13 17:55:21 +0200

    Delete Assets/SuperTiled2Unity directory

--- Assets/SuperTiled2Unity/Import/Road_Tiles_pal.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="Road_Tiles_pal" tilewidth="128" tileheight="128" tilecount="324" columns="18">
- <image source="Road_Tiles.png" width="2304" height="2304"/>
-</tileset>
